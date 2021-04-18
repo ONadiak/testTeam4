be0ad7917896d5a0b6606731902dbe00765992c5
maxymsemochko <81555113+maxymsemochko@users.noreply.github.com>
2021-04-18 22:07:09 +0300

    Deleted useless code

--- src/test/java/com/automationPractice/PageObjects/MainPage.java
+++ src/test/java/com/automationPractice/PageObjects/MainPage.java
@@ -1,30 +1,4 @@
 package com.automationPractice.PageObjects;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
 public class MainPage extends BasePage {
-
-
-
-    @FindBy(xpath = "//a[@class='dropdown-toggle']")
-    private WebElement myAccountButton;
-    @FindBy(xpath = "//*[text()='Register'][1]")
-    private WebElement registrationButton;
-
-    public WebElement getMyAccountButton() {
-        wait.until(ExpectedConditions.visibilityOf(myAccountButton));
-        return myAccountButton;
-    }
-    public WebElement getRegistrationButton() {
-        return registrationButton;
-    }
-
-
-
-
-
-
-
 }
